c84ac31d2d29ad5db860b2350ad03b10767062e1
Kyle Szklenski <kyle.szklenski@gmail.com>
2020-01-07 22:41:40 -0500

    Adding plugin capability

--- swipe_detector/SwipeDetector.gd
+++ swipe_detector/SwipeDetector.gd
@@ -1,14 +1,17 @@
 extends Node
+class_name SwipeDetector
 
 signal swiped(direction)
 
 export (float) onready var MinSpeed
 
-var children
+export (PoolVector2Array) var swipe_directions
+export (float) var AngleToleranceInDegrees
 var touch_down
+var rad_tol
 
 func _ready() -> void:
-    children = $Directions.get_children()
+    rad_tol = deg2rad(AngleToleranceInDegrees)
 
 func _unhandled_input(event: InputEvent) -> void:
         if event is InputEventScreenTouch:
@@ -16,8 +19,14 @@ func _unhandled_input(event: InputEvent) -> void:
                 touch_down = event
             elif not event.pressed and touch_down:
                 var difference = event.position - touch_down.position
-                for child in children:
-                    if child.applies(difference):
-                        emit_signal("swiped", child.SwipeDirection)
+                for child in swipe_directions:
+                    if applies_to(child, difference):
+                        emit_signal("swiped", child)
                         break
                 touch_down = null
+
+func applies_to(direction : Vector2, relative_direction : Vector2) -> bool:
+    var upper_bound = direction.angle() + rad_tol
+    var lower_bound = direction.angle() - rad_tol
+    var rel_angle = relative_direction.normalized().angle()
+    return lower_bound < rel_angle and rel_angle < upper_bound
--- swipe_detector/plugin.gd
+++ swipe_detector/plugin.gd
@@ -2,7 +2,7 @@ tool
 extends EditorPlugin
 
 func _enter_tree():
-    pass
+    add_custom_type("SwipeDetector", "Node", preload("res://addons/swipe_detector/SwipeDetector.gd"), preload("res://addons/swipe_detector/icon.png"))
 
 func _exit_tree():
-    pass
+    remove_custom_type("SwipeDetector")
