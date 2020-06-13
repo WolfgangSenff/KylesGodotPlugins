59e745d957b5fc59a9c6ee2c8205eaaf6be866f4
Kyle Szklenski <kyle.szklenski@gmail.com>
2020-06-13 13:20:45 -0400

    Fix for trail issues

--- line_trail/plugin.gd
+++ line_trail/plugin.gd
@@ -3,9 +3,8 @@ extends EditorPlugin
 
 
 func _enter_tree() -> void:
-    add_custom_type("LineTrail", "Node", )
-    pass
+    add_custom_type("LineTrail", "Node", preload("res://addons/line_trail/LineTrail.gd"), preload("res://addons/line_trail/icon.png"))
 
 
 func _exit_tree() -> void:
-    pass
+    remove_custom_type("LineTrail")
